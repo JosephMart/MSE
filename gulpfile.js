5c751abb79b20cdd022113af3d24dca449c79a4a
Joseph Martinsen <joseph@martinsen.com>
2017-03-08 19:01:58 -0600

    Gulp cleaned up

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@
 const gulp  = require('gulp'),
     browserSync = require('browser-sync').create(),
     htmlmin = require('gulp-htmlmin'),
-    nunjucksRender = require('gulp-nunjucks-render'); // importing the plugin
+    nunjucksRender = require('gulp-nunjucks-render');
     image = require('gulp-image');
 
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
+var del = require('del');
 
 const PATHS = {
     output: 'dist',
@@ -15,7 +16,8 @@ const PATHS = {
     styles: 'src/sass',
     css: 'src/css',
     fonts: 'src/fonts',
-    img: 'src/images'
+    img: 'src/images',
+    fonts: 'src/fonts'
 }
 
 
@@ -28,21 +30,30 @@ gulp.task('image', function () {
 gulp.task('fonts', function(){
   console.log('Moving fonts...');
   return gulp.src(PATHS.fonts + "/.*")
-      .pipe(gulp.dest(PATHS.output + '/fonts'));
+      .pipe(gulp.dest(PATHS.output + '/fonts'))
+      .pipe(browserSync.reload({
+        stream: true
+    }));
 });
 
 gulp.task('minify-css', function() {
   console.log('Minifying CSS files..');
   return gulp.src(PATHS.css + '/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest( PATHS.output + '/css'));
+    .pipe(gulp.dest( PATHS.output + '/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 gulp.task('sass', function () {
   console.log('Rendering SASS files..');
   return gulp.src(PATHS.styles + '/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(PATHS.output + '/css'));
+    .pipe(gulp.dest(PATHS.output + '/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
 // writing up the gulp nunjucks task
@@ -53,7 +64,10 @@ gulp.task('nunjucks', function() {
           path: [PATHS.templates],
           watch: true,
         }))
-        .pipe(gulp.dest(PATHS.output));
+        .pipe(gulp.dest(PATHS.output))
+        .pipe(browserSync.reload({
+          stream: true
+        }));
 });
 
 gulp.task('browserSync', function() {
@@ -64,14 +78,20 @@ gulp.task('browserSync', function() {
     });
 });
 
-gulp.task('watch', function() {
+gulp.task('fonts', function() {
+  return gulp.src(PATHS.fonts + '/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+
+gulp.task('watch',['browserSync', 'sass', 'minify-css'], function() {
     // trigger Nunjucks render when pages or templates changes
     gulp.watch([PATHS.pages + '/**/*.+(html|js|css)', PATHS.templates + '/**/*.+(html|js|css)'], ['nunjucks'])
     gulp.watch(PATHS.styles + '/*.scss', ['sass']);
     gulp.watch(PATHS.css + '/*.css', ['minify-css']);
     gulp.watch(PATHS.img + '/**', ['image']);
     // reload browsersync when `dist` changes
-    gulp.watch(PATHS.output + '/**').on('change', browserSync.reload);
+    // gulp.watch(PATHS.output + '/**').on('change', browserSync.reload);
 });
 
 gulp.task('minify', function() {
@@ -86,8 +106,14 @@ gulp.task('minify', function() {
     .pipe(gulp.dest(PATHS.output));
 });
 
+gulp.task('clean:dist', function() {
+  return del.sync(PATHS.output);
+})
+
 // run browserSync auto-reload together with nunjucks auto-render
 gulp.task('auto', ['browserSync', 'watch']);
 
-//default task to be run with gulp
-gulp.task('default', ['nunjucks']);
+// Build all
+gulp.task('build', [`clean`, `sass`, `minify-css`, `image`, `nunjucks`], function (){
+  console.log('Building all files');
+})
